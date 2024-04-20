fca0101c7a935a709c85e02e8e5682406e104956
Bird <birdmandayum0131@gmail.com>
2024-04-20 23:16:42 +0800

    refactor: Add domain layer (clean architecture)
    add url entry/repository entity to domain layer
    entity in the layer not be called yet

---
+++ domain/url.go
@@ -0,0 +1,13 @@
+package domain
+
+// Repository that store the entry of url mapping
+type URLRepository interface {
+	Store(entry URLEntry) error
+	Get(shortURL string) (URLEntry, error)
+}
+
+// Entry of url mapping task
+type URLEntry struct {
+	ShortURL string
+	LongURL  string
+}
