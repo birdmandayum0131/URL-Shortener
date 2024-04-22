65c538b19a3cdd6a0a40d6c2b69fd7b9b7bf65e4
Bird <birdmandayum0131@gmail.com>
2024-04-23 04:02:03 +0800

    handler: refactor url shorten handler
    - merge url shorten handler to one file
    - refactor code snippets to call url shorten service

--- handlers/url/handler.create.go
+++ handlers/url/handler.create.go
@@ -1,57 +1,55 @@
 package handlers
 
 import (
-	"fmt"
-	"math/big"
 	"net/http"
+	schemas "shortener/schemas/api"
+	"shortener/services"
 
-	"github.com/bwmarrin/snowflake"
 	"github.com/gin-gonic/gin"
-	"github.com/jmoiron/sqlx"
 )
 
-type shorten struct {
-	ID       int64  `json:"id" db:"id"`
-	LongURL  string `json:"longUrl" db:"longURL"`
-	ShortURL string `json:"shortUrl" db:"shortURL"`
+// class to handle url shorten tasks
+type URLHandler struct {
+	URLInteractor services.URLEntryInteractor
+	Logger        Logger
 }
 
-var UrlDB *sqlx.DB
-var IdNode *snowflake.Node
-
-// postShorten adds a shorten url from JSON received in the request body.
-func CreateUrlHandler(c *gin.Context) {
-	var newShorten shorten
-	// Call BindJSON to bind the received JSON to
-	// newShorten.
-	if err := c.Bind(&newShorten); err != nil {
-		println(err)
+// Create a new url entry
+func (handler *URLHandler) CreateURLHandler(c *gin.Context) {
+	var request schemas.CreateURLRequest
+	// Call BindJSON to bind the received JSON request
+	if err := c.Bind(&request); err != nil {
+		handler.Logger.Log(err.Error())
 		return
 	}
 
-	// * get url id
-	results, err := UrlDB.Queryx(fmt.Sprintf("SELECT id, shortURL, longURL FROM url WHERE longURL = '%s'", newShorten.LongURL))
+	shortURL, err := handler.URLInteractor.CreateEntry(request.LongURL)
 	if err != nil {
-		panic(err.Error())
+		handler.Logger.Log(err.Error())
+		return
 	}
 
-	if results.Next() {
-		results.StructScan(&newShorten)
-	} else {
-		id := IdNode.Generate()
-		newShorten.ID = id.Int64()
-		var base62Int big.Int
-		base62Int.SetInt64(newShorten.ID)
-		newShorten.ShortURL = base62Int.Text(62)
-		sqlStatement := `
-INSERT INTO url (id, shortURL, longURL)
-VALUES (:id, :shortURL, :longURL)`
-		_, err = UrlDB.NamedExec(sqlStatement, newShorten)
-		if err != nil {
-			panic(err)
-		}
+	response := schemas.CreateURLResponse{
+		LongURL:  request.LongURL,
+		ShortURL: shortURL,
 	}
 
-	// Add the new album to the slice.
-	c.IndentedJSON(http.StatusCreated, newShorten)
+	c.IndentedJSON(http.StatusCreated, response)
+}
+
+func (handler *URLHandler) GetURLHandler(c *gin.Context) {
+	shortURL := c.Param("hash")
+
+	longURL, err := handler.URLInteractor.GetURL(shortURL)
+	if err != nil {
+		handler.Logger.Log(err.Error())
+		return
+	}
+
+	response := schemas.GetURLResponse{
+		LongURL:  longURL,
+		ShortURL: shortURL,
+	}
+
+	c.IndentedJSON(http.StatusFound, response)
 }
