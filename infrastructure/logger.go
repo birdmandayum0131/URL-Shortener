fa0cf05d1418dc5e0969b30736805091e3ee6531
Bird <birdmandayum0131@gmail.com>
2024-04-23 04:05:47 +0800

    fix: fix some mistake about private/public properties in struct

--- infrastructure/logger.go
+++ infrastructure/logger.go
@@ -1,6 +1,15 @@
 package infrastructure
 
+import "fmt"
+
 // Simple Logger for log messages in usecase layer
 type Logger interface {
 	Log(message string) error
+}
+
+type SimpleStdLogger struct{}
+
+func (l *SimpleStdLogger) Log(message string) error {
+	_, err := fmt.Println(message)
+	return err
 }
