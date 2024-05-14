78232227d1a39fd6f956380cec21ee8d8c92f00a
Bird <birdmandayum0131@gmail.com>
2024-05-14 15:18:56 +0800

    example: merge docker-build to docker-run shell script

--- examples/docker/docker-build.sh
+++
@@ -1,2 +0,0 @@
-docker system prune --all --force
-docker build -f ./docker/Dockerfile -t url-shortener .
--- examples/docker/docker-run.sh
+++ examples/docker/docker-run.sh
@@ -1,1 +1,2 @@
+docker system prune --all --force
 docker compose -f ./examples/docker/docker-compose.yaml up -d --force-recreate #--build
